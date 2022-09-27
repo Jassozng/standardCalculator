0297b1978d236806a60ac21a853952f95e05aec5
jasso <jassozng@gmail.com>
2022-09-26 19:11:03 -0500

    Virtal pad mapping, keymapping and github repo href done

--- build/js/index.js
+++ build/js/index.js
@@ -1,49 +1,107 @@
 window.onload = () => {
-    const calculatorContainer = document.getElementById("body");
-    const operationBox = document.getElementById("operations");
-    mapping(calculatorContainer, operationBox);
+    keyboardMapping();
+    virtualPadMapping();
 };
 
 
-const mapping = (eventTarget, containerTarget) => {
-    let results = document.getElementById("results");
-    eventTarget.addEventListener("keydown", (event) => {
+const keyboardMapping = () => {
+    const results = document.getElementById("results");
+    const operationBox = document.getElementById("operations");
+    document.getElementById("body").addEventListener("keydown", (event) => {
         switch(event.keyCode){
             case 8: 
-                containerTarget.value = containerTarget.value.slice(0, -1);
+                operationBox.value = operationBox.value.slice(0, -1);
                 break;
             case 13: 
-                const strOperation = results.value + " " + containerTarget.value + " =";
-                containerTarget.value = solveOperation(strOperation);
+                const strOperation = results.value + " " + operationBox.value + " =";
+                operationBox.value = solveOperation(strOperation);
                 results.value = strOperation;
                 break;
+            case 46:
             case 106:
             case 107:
             case 109:
             case 111:
-                if(results.value != ""){
-                    const strOperation = results.value + " " + containerTarget.value + " =";
-                    const solution = solveOperation(strOperation);
-                    results.value = solution + " " + event.key;
-                    containerTarget.value = "";
-                }else{
-                    results.value = containerTarget.value + " " + event.key;
-                    containerTarget.value = "";
-                }
+                const appendingValues = operationKeyDownEvent(results.value, operationBox.value, event.key);
+                results.value = appendingValues[0];
+                operationBox.value = appendingValues[1];
                 break;
             default:
-                if(event.shiftKey != true){
-                    const regex = /\d/;
-                    const found = event.key.match(regex);
-                    if(found != null && containerTarget.value.length < 16){
-                        results.value.charAt(results.value.length - 1) == "=" ? (containerTarget.value = event.key, results.value = "") : containerTarget.value += event.key;
-                    }
-                    break;
-                }else{
-
-                }
+                const values = numberKeyDownEvent(results.value, operationBox.value, event.key);
+                results.value = values[0];
+                operationBox.value = values[1];
+                break;
         };
     });
+};
+
+const virtualPadMapping = () => {
+    const results = document.getElementById("results");
+    const operationBox = document.getElementById("operations");
+    const numberButtons = document.querySelectorAll("#number");
+    const operationButtons = document.querySelectorAll("#operator");
+    numberButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const appendingValues = numberKeyDownEvent(results.value, operationBox.value, button.value);
+            results.value = appendingValues[0];
+            operationBox.value = appendingValues[1];
+        });
+    });
+    operationButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const appendingValues = operationKeyDownEvent(results.value, operationBox.value, button.value);
+            results.value = appendingValues[0];
+            operationBox.value = appendingValues[1];
+        });
+    });
+};
+
+const operationKeyDownEvent = (results, operationBox, key) => {
+    switch(key){
+        case "CE":
+        case "Delete":
+            results = "";
+            operationBox = "";
+            break;
+        case "C":
+            operationBox = "";
+            break;
+        case "←": 
+            operationBox = operationBox.slice(0, -1);
+            break;
+        case "±": 
+            operationBox = -operationBox;
+            break;
+        case "⅟": 
+            operationBox = 1/(operationBox);
+            break;
+        case "√x": 
+            operationBox = Math.sqrt(operationBox);
+            break;
+        case ".": 
+            operationBox.match(/[.]/) == null ? operationBox += "." : null;
+            break;
+        default:
+            if(results.match(/[-+*⅟×±÷]/) != null){
+                const strOperation = results + " " + operationBox + " =";
+                const solution = solveOperation(strOperation);
+                results = solution + " " + key;
+                operationBox = "";
+            }else{
+                results = operationBox + " " + key;
+                operationBox = "";
+            };
+    }
+    return [results, operationBox];
+};
+
+const numberKeyDownEvent = (results, operationBox, key) => {
+    const regex = /\d/;
+    const found = key.match(regex);
+    if(found != null && operationBox.length < 16){
+        results.charAt(results.length - 1) == "=" ? (operationBox = key, results = "") : operationBox += key;
+    }
+    return [results, operationBox];
 };
 
 const solveOperation = (strOperation) => {
@@ -51,6 +109,6 @@ const solveOperation = (strOperation) => {
     const x = Number(splitOperation[0]);
     const y = Number(splitOperation[2]);
     const operation = splitOperation[1];
-    const solverObj = {"+": x+y, "-": x-y, "*": x*y, "/": x/y};
+    const solverObj = {"+": x+y, "-": x-y, "*": x*y, "×": x*y, "/": x/y, "%": x%y, "÷": x/y };
     return solverObj[operation];
 }
